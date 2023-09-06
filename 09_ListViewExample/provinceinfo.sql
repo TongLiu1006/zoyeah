f82f5354576eadf7ee3dc3a3b582b1c7e00f53e5
TongLiu1006 <61133409+TongLiu1006@users.noreply.github.com>
2023-09-06 22:50:27 +0800

    9.6info

---
+++ 09_ListViewExample/provinceinfo.sql
@@ -0,0 +1,2 @@
+INSERT INTO `provinceinfo`(`ProvinceID`, `ProvinceName`) VALUES (0, 'Shanghai');
+INSERT INTO `provinceinfo`(`ProvinceID`, `ProvinceName`) VALUES (1, 'Wuxi');
---
+++ 09_ListViewExample/s_info.sql
@@ -0,0 +1,2 @@
+INSERT INTO `s_info`(`ContactID`, `FirstName`, `LastName`, `EmailAddress`, `ProvinceID`, `UpdateTime`) VALUES (1, 'liu', 'tong', '1941372552@gmail.com', 1, '2023-09-06 20:35:26');
+INSERT INTO `s_info`(`ContactID`, `FirstName`, `LastName`, `EmailAddress`, `ProvinceID`, `UpdateTime`) VALUES (2, 'li', 'si', '2287416145.qqxxcom', 0, '2023-09-06 20:35:29');
