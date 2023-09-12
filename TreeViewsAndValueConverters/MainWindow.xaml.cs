970c66f204b62e966508a87eeb580b3c847161f6
TongLiu1006 <61133409+TongLiu1006@users.noreply.github.com>
2023-09-12 18:42:06 +0800

    9.12
    TreeViewAndConverters

---
+++ TreeViewsAndValueConverters/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewsAndValueConverters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //var t=new TreeViewItem();
+            //t.HeaderTemplate
+        }
+        #endregion constructor
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+
+                var item = new TreeViewItem();
+                FolderView.Items.Add(item);
+            }
+
+            
+        }
+    }
+}
